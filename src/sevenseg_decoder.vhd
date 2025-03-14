1f898d92eee8937761a207e7b81dd23486bfc700
GageSpencer1 <c27gage.spencer@afacademy.af.edu>
2025-03-13 23:55:37 -0600

    Somewhat working. Need help

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -32,31 +32,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+   port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
            o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
 
-    signal w_seg : std_logic_vector(6 downto 0):= (others => '0');
+    signal w_seg : std_logic_vector(6 downto 0) := (others => '0');
 
 begin
     with i_Hex select
-    w_seg   <= "1000000" when "0000", -- 1
-               "1111001" when "0001", -- 2
-               "0100100" when "0010", -- 3
-               "0110000" when "0011", -- 4 
-               "0010010" when "0100", -- 5
-               "0000010" when "0101", -- 6
-               "1111000" when "0110", -- 7
-               "0000000" when "0111", -- 8
-               "0011000" when "1000", -- 9
-               "0001000" when "1001", -- A
-               "0000011" when "1010", -- B
-               "0100111" when "1011", -- C
-               "0100001" when "1100", -- D
-               "0000110" when "1101", -- E
-               "0001110" when "1111", -- F
-               "0000000" when others;
-
+    w_seg   <= "1000000" when x"0", -- 0
+               "1111001" when x"1", -- 1
+               "0100100" when x"2", -- 2
+               "0110000" when x"3", -- 3 
+               "0011001" when x"4", -- 4
+               "0010010" when x"5", -- 5
+               "0000010" when x"6", -- 6
+               "1111000" when x"7", -- 7
+               "0000000" when x"8", -- 8
+               "0011000" when x"9", -- 9
+               "0001000" when x"A", -- A
+               "0000011" when x"B", -- B
+               "0100111" when x"C", -- C
+               "0100001" when x"D", -- D
+               "0000110" when x"E", -- E
+               "0001110" when x"F", -- F
+               "1111111" when others;
+        o_seg_n <= w_seg;
 end Behavioral;
